79392921de5b2b9a2af015b3f209b907e732e0a2
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-01-18 23:10:56 -0300

    último desafio a ser feito foi o de advinha e falta o de número primo

---
+++ script.js
@@ -0,0 +1,62 @@
+// Desafio 1 - Fácil: Cálculo de Média Simples
+
+// let notaUm = parseFloat(prompt('Digite a primeira nota de 0 a 10'));
+// let notaDois = parseFloat(prompt('Digite a segunda nota 0 a 10'));
+// let notaTres = parseFloat(prompt('Digite a terceira nota 0 a 10'));
+
+// let media = (notaUm + notaDois + notaTres) / 3;
+
+// if (media >= 7) {
+//     document.write(`Aluno aprovado com média ${media.toFixed()}!`);
+// } else {
+//     document.write(`Aluno reprovado com média ${media.toFixed()}!`);
+// }
+
+// let notaUm = parseFloat(prompt('Digite a primeira nota.'));
+// if (notaUm < 0 || notaUm > 10 ) {
+//     alert('Insira uma nota entre 0 e 10.');
+//     notaUm = parseFloat(prompt('Digite a primeira nota.'));
+// }
+// let notaDois = parseFloat(prompt('Digite a segunda nota.'));
+// if (notaDois < 0 || notaDois > 10) {
+//     alert('Insira uma nota entre 0 e 10.');
+//     notaDois = parseFloat(prompt('Digite a segunda nota.'));
+// }
+// let notaTres = parseFloat(prompt('Digite a terceira nota.'));
+// if (notaTres < 0 || notaTres > 10 ) {
+//     alert('Insira uma nota entre 0 e 10.');
+//     notaTres = parseFloat(prompt('Digite a terceira nota.'));
+// }
+// let media = (notaUm + notaDois + notaTres) / 3;
+// if (media >= 7) {
+//     document.write(`Aluno está aprovado com média ${media.toFixed()}.`);
+// } else {
+//     document.write(`Aluno reprovado com média ${media.toFixed()}.`);
+// }
+
+// Desafio 1 - Fácil: Par ou Ímpar
+
+// let numeros = parseInt(prompt('Digite um número inteiro'))
+
+// if (numeros % 2 === 0) {
+//     document.write('Este número é par');
+// } else {
+//     document.write('Este número é ímpar');
+// }
+
+// ------------------------------------------------------//
+
+// Desafio 2 - Médio: Números Primos
+
+
+
+
+// Desafio 2 - Médio: Jogo de Adivinhação
+
+// https://lua-adivinha.vercel.app/
+
+
+
+
+
+
